5a58d2b36f723539e885e3fed19e86ed56d16692
Nemanja Masnikosa <neman93@live.com>
2018-06-03 01:02:50 +0200

    Initial commit

---
+++ src/main/java/com/oop2/tim6/NakitWebTim6/NakitWebTim6Application.java
@@ -0,0 +1,12 @@
+package com.oop2.tim6.NakitWebTim6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakitWebTim6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NakitWebTim6Application.class, args);
+	}
+}
---
+++ src/test/java/com/oop2/tim6/NakitWebTim6/NakitWebTim6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oop2.tim6.NakitWebTim6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NakitWebTim6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
