c0f14493d5bc21fcbbbe0c3d3f7cb3ab153fd9ef
mirkovicmiroslav <dmi.miroslav.mirkovic@student.pmf.uns.ac.rs>
2018-06-14 23:28:56 +0200

    Quick fix

--- src/main/java/com/oop2/tim6/NakitWebTim6/NakitWebTim6Application.java
+++ src/main/java/com/oop2/tim6/NakitWebTim6/NakitWebTim6Application.java
@@ -2,19 +2,12 @@ package com.oop2.tim6.NakitWebTim6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.oop2.tim6.NakitWebTim6.repository")
-@EntityScan("com.oop2.tim6.NakitWebTim6.model")
-@ComponentScan({"com.oop2.tim6.NakitWebTim6.controller", "com.oop2.tim6.NakitWebTim6.service"})
 @SpringBootApplication
 public class NakitWebTim6Application  {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(NakitWebTim6Application.class, args);
     }
-
 
 }
